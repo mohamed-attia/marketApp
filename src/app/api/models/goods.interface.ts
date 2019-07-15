97136b3fd45ac483ac0360373d24ac74b8edcce3
mohamed--attia <m.attia@outlook.sa>
2019-07-15 16:12:37 +0200

    working on home page

---
+++ src/app/api/models/goods.interface.ts
@@ -0,0 +1,5 @@
+export interface GoodModel {
+    name?: string;
+    price?: string;
+    photoUrl?: string;
+}
--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { GoodModel } from "src/app/api/models/goods.interface";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  selector: "app-home",
+  templateUrl: "./home.component.html",
+  styleUrls: ["./home.component.css"]
 })
 export class HomeComponent implements OnInit {
+  public goods: GoodModel[] = [
+    {
+      name: "orange",
+      price: "5",
+      photoUrl: "assets/imgs/1.jpg"
+    },
+    {
+      name: "apple",
+      price: "6",
+      photoUrl: "assets/imgs/2.jpg"
+    },
+    {
+      name: "kiwi",
+      price: "7",
+      photoUrl: "assets/imgs/3.jpg"
+    },
+    {
+      name: "strawberry",
+      price: "7",
+      photoUrl: "assets/imgs/4.jpg"
+    }
+  ];
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  public addtoCart(i){
+    console.log(i)
   }
-
 }
